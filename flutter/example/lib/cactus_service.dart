c388a12d990b17642ffc82bb6ae7722207b93bb6
rshemet <shemet0roman@gmail.com>
2025-07-07 14:31:44 -0700

    typo fix

--- flutter/example/lib/cactus_service.dart
+++ flutter/example/lib/cactus_service.dart
@@ -18,7 +18,7 @@ class CactusService {
     try {
       model = await CactusVLM.init(
         modelUrl: 'https://huggingface.co/Cactus-Compute/SmolVLM2-500m-Instruct-GGUF/resolve/main/SmolVLM2-500M-Video-Instruct-Q8_0.gguf',
-        visionUrl: 'https://huggingface.co/Cactus-Compute/SmolVLM2-500m-Instruct-GGUF/resolve/main/mmproj-SmolVLM2-500M-Video-Instruct-Q8_0.ggu',
+        visionUrl: 'https://huggingface.co/Cactus-Compute/SmolVLM2-500m-Instruct-GGUF/resolve/main/mmproj-SmolVLM2-500M-Video-Instruct-Q8_0.gguf',
         onProgress: (progress, statusText, isError) {
           status.value = statusText;
           if (isError) error.value = statusText;
